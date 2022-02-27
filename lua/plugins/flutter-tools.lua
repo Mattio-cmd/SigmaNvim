23b38dd184a73a3ec0b372b5d51d2e3318a891ed
Mattio-cmd <coghiqm8@gmail.com>
2022-02-26 22:55:55 -0600

    Finally fixed indent-blankline

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -1,1 +1,33 @@
-require("flutter-tools").setup{} -- use defaults
+require("flutter-tools").setup{
+    ui = {
+    border = "rounded",
+    -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+    -- please note that this option is eventually going to be deprecated and users will need to
+    -- depend on plugins like `nvim-notify` instead.
+    notification_style = 'native' or 'plugin'
+  },
+    decorations = {
+    statusline = {
+      -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+      -- this will show the current version of the flutter app from the pubspec.yaml file
+      app_version = false,
+      -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+      -- this will show the currently running device if an application was started with a specific
+      -- device
+      device = true,
+    }
+  },
+  dev_log = {
+    enabled = true,
+    open_cmd = "tabedit", -- command to use to open the log buffer
+  },
+   lsp = {
+    color = { -- show the derived colours for dart variables
+      enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+      background = true, -- highlight the background
+      foreground = false, -- highlight the foreground
+      virtual_text = true, -- show the highlight using virtual text
+      virtual_text_str = "■", -- the virtual text character to highlight
+    },
+  } -- use defaults
+}
