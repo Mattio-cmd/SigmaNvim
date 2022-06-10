5646843652f9a37fdfc02c90adb3627a31d088d8
Mattio-cmd <coghiqm8@gmail.com>
2022-06-09 18:10:13 -0600

    Modified stuff

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -19,8 +19,8 @@
   }
 
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+--vim.opt.listchars:append("space:⋅")
+--vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
     --space_char_blankline = " ",
