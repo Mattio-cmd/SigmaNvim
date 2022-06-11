2994e6b74e1c0bb1845adced0b21b4c55de3f392
Mattio-cmd <coghiqm8@gmail.com>
2022-06-10 19:02:53 -0600

    Added a LaTeX template

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -101,7 +101,8 @@ nnoremap ,h :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.html<CR>3jwf>a
 nnoremap ,c :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.c<CR>4ji<TAB>
 nnoremap ,f :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.dart<CR>2j6li
 nnoremap ,m :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.make<CR>j4li
-nnoremap ,t :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.tex<CR>1j6l<S-a><Space>
+nnoremap ,t :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.tex<CR>2j9li
+nnoremap ,d :-1read $HOME/.config/nvim/lua/skeletons/.skeleton.md<CR>1j6l<S-a><Space>
 
 " For flutter development
 nnoremap ;f :FlutterOutlineToggle<CR>
