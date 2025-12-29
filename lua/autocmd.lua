ea604e655e2114b717111bfa040b348860710b10
Mattio-cmd <mattioc@protonmail.com>
2025-12-29 10:41:54 -0600

    Migrated autocmd.lua to lua instead of vimscript

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,108 +1,160 @@
-local cmd = vim.cmd
-local exec = vim.api.nvim_exec
+local api = vim.api
+
+-- Helper: augroup
+local function augroup(name)
+  return api.nvim_create_augroup(name, { clear = true })
+end
 
 -- Remove trailing whitespace on save
-local NoWhitespace = exec([[
-function! NoWhitespace()
-    let l:save = winsaveview()
-    keeppatterns %s/\s\+$//e
-    call winrestview(l:save)
-endfunction
-call NoWhitespace()
-]], true)
+local function trim_trailing_whitespace()
+  local view = vim.fn.winsaveview()
+  vim.cmd([[keeppatterns %s/\s\+$//e]])
+  vim.fn.winrestview(view)
+end
 
-exec([[au BufWritePre * call NoWhitespace()]], false)
+api.nvim_create_autocmd("BufWritePre", {
+  group = augroup("trim_whitespace"),
+  callback = trim_trailing_whitespace,
+})
 
--- Disable comments in format options
-exec([[au BufEnter * set fo-=c fo-=r fo-=o]], false)
+-- Disable automatic comment insertion
+api.nvim_create_autocmd("BufEnter", {
+  group = augroup("format_options"),
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
 
 -- Terminal settings
-cmd([[
-augroup terminal
-    autocmd!
-    autocmd TermOpen * startinsert
-    autocmd TermOpen * setlocal nonumber norelativenumber
-    autocmd TermOpen * tnoremap <Esc> <c-\><c-n> <cmd>bd!<CR>
-augroup END
-]])
+api.nvim_create_autocmd("TermOpen", {
+  group = augroup("terminal"),
+  callback = function()
+    vim.cmd("startinsert")
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+
+    vim.keymap.set("t", "<Esc>", [[<C-\><C-n><cmd>bd!<CR>]], {
+      buffer = true,
+      silent = true,
+    })
+  end,
+})
 
 -- Filetype-specific settings
-cmd([[
-augroup filetype_settings
-    autocmd!
-    autocmd FileType lua setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2
-    autocmd FileType html setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4
-    autocmd FileType scss setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4
-augroup END
-]])
+api.nvim_create_autocmd("FileType", {
+  group = augroup("filetype_settings"),
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+api.nvim_create_autocmd("FileType", {
+  group = augroup("filetype_settings"),
+  pattern = { "html", "scss" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
 
 -- Restore cursor position on file open
-cmd([[
-augroup last_cursor_position
-    autocmd!
-    autocmd BufReadPost *
-        \ if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit' |
-        \ execute "normal! g`\"zvzz" | endif
-augroup END
-]])
+api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_cursor_position"),
+  callback = function()
+    local mark = api.nvim_buf_get_mark(0, '"')
+    local lcount = api.nvim_buf_line_count(0)
+
+    if mark[1] > 1 and mark[1] <= lcount and vim.bo.filetype ~= "commit" then
+      vim.cmd([[normal! g`"zvzz]])
+    end
+  end,
+})
 
 -- Reload file if changed externally
-cmd([[
-augroup auto_reload_file
-    autocmd!
-    autocmd FileChangedShellPost * call v:lua.vim.notify("File changed on your device. Buffer reload! Process completed!", 'warn', {'title': 'nvim'})
-    autocmd FocusGained,CursorHold * if getcmdwintype() == '' | checktime | endif
-augroup END
-]])
+api.nvim_create_autocmd("FileChangedShellPost", {
+  group = augroup("auto_reload_file"),
+  callback = function()
+    vim.notify(
+      "File changed on your device. Buffer reload completed.",
+      vim.log.levels.WARN,
+      { title = "nvim" }
+    )
+  end,
+})
 
--- Toggle relative line numbers based on window focus
-cmd([[
-augroup numbertoggle
-    autocmd!
-    autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu | set rnu | endif
-    autocmd BufLeave,FocusLost,InsertEnter,WinLeave * if &nu | set nornu | endif
-augroup END
-]])
+api.nvim_create_autocmd({ "FocusGained", "CursorHold" }, {
+  group = augroup("auto_reload_file"),
+  callback = function()
+    if vim.fn.getcmdwintype() == "" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 
--- Enable spell checking for specific file types
-vim.api.nvim_create_autocmd(
-    { "BufRead", "BufNewFile" },
-    { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
+-- Toggle relative line numbers
+api.nvim_create_autocmd(
+  { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+  {
+    group = augroup("numbertoggle"),
+    callback = function()
+      if vim.wo.number then
+        vim.wo.relativenumber = true
+      end
+    end,
+  }
 )
 
--- Highlight text after yanking
-cmd([[
-augroup yank_with_highlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-augroup END
-]])
+api.nvim_create_autocmd(
+  { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+  {
+    group = augroup("numbertoggle"),
+    callback = function()
+      if vim.wo.number then
+        vim.wo.relativenumber = false
+      end
+    end,
+  }
+)
 
--- Nvim-cmp highlights
-cmd([[
-augroup cmp_highlights
-    autocmd!
-    highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
-    highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
-    highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
-    highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
-    highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
-    highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
-    highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
-    highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
-    highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
-    highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
-    highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
-augroup END
-]])
+-- Enable spell checking for text files
+api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("spellcheck"),
+  pattern = { "*.txt", "*.md", "*.tex" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
 
--- Optional: Floating diagnostic for LSP (commented out)
--- cmd [[
--- autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})
--- ]]
+-- Highlight yanked text
+api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("yank_highlight"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
--- Dart LSP (commented out)
--- cmd [[
--- let g:lsc_auto_map = v:true
--- ]]
+-- nvim-cmp highlights (colorscheme-safe)
+api.nvim_create_autocmd("ColorScheme", {
+  group = augroup("cmp_highlights"),
+  callback = function()
+    local set = vim.api.nvim_set_hl
 
+    set(0, "CmpItemAbbrDeprecated", { fg = "#808080", strikethrough = true })
+    set(0, "CmpItemAbbrMatch", { fg = "#569CD6" })
+    set(0, "CmpItemAbbrMatchFuzzy", { fg = "#569CD6" })
+    set(0, "CmpItemKindVariable", { fg = "#9CDCFE" })
+    set(0, "CmpItemKindInterface", { fg = "#9CDCFE" })
+    set(0, "CmpItemKindText", { fg = "#9CDCFE" })
+    set(0, "CmpItemKindFunction", { fg = "#C586C0" })
+    set(0, "CmpItemKindMethod", { fg = "#C586C0" })
+    set(0, "CmpItemKindKeyword", { fg = "#D4D4D4" })
+    set(0, "CmpItemKindProperty", { fg = "#D4D4D4" })
+    set(0, "CmpItemKindUnit", { fg = "#D4D4D4" })
+  end,
+})
