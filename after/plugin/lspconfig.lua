941356187717c22c164d71db192332932e227737
Mattio-cmd <mattioc@protonmail.com>
2026-04-03 23:45:43 -0600

    Added LS

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,47 +1,7 @@
 -- Set LSP log level to "debug"
-vim.lsp.set_log_level("debug")
+--vim.lsp.set_log_level("debug")
 
--- Python LSP: Pyright
-vim.lsp.config('pyright', {})
-
--- Lua LSP: lua_ls with specific settings
-vim.lsp.config('lua_ls', {
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = {
-            version = 'LuaJIT'
-          },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME
-            }
-          }
-        }
-      })
-      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-    end
-    return true
-  end
-})
-
--- HTML LSP with completion and embedded languages support
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-vim.lsp.config('html', {
-  capabilities = capabilities,
-  configurationSection = { "html", "css", "javascript" },
-  embeddedLanguages = { css = true, javascript = true },
-  provideFormatter = true
-})
-
--- C/C++ LSP: clangd
-vim.lsp.config('clangd', {})
-
--- LaTeX LSP: texlab
+-- LaTeX LSP: texlab (This works)
 vim.lsp.enable('texlab')
 vim.lsp.config('texlab', {
   texlab = {
@@ -65,19 +25,25 @@ vim.lsp.config('texlab', {
   }
 })
 
--- SystemVerilog LSP: verible
-vim.lsp.config('verible', {})
+vim.lsp.enable('clangd')
+vim.lsp.enable('svls')
+vim.lsp.enable('asm_lsp')
 
--- Rust LSP: rust_analyzer
-vim.lsp.config('rust_analyzer', {})
+vim.lsp.config('lua_ls', {
+  single_file_support = true,
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
 
--- TypeScript LSP: ts_ls (TypeScript Language Server)
-vim.lsp.config('ts_ls', {})
-
--- CSS LSP: cssls
-vim.lsp.config('cssls', {
-  capabilities = capabilities,
+  root_markers = {
+    '.lua',
+    '.git',
+    '.luarc.json',
+    '.luarc.jsonc'
+  },
 })
+vim.lsp.enable('lua_ls')
+
+
 
 -- Use a custom on_attach function to bind keymaps for LSP actions
 local on_attach = function(client, bufnr)
@@ -104,27 +70,5 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
-  -- Disable formatting for tsserver
-  --if client.name == 'tsserver' then
-    --client.resolved_capabilities.document_formatting = false
-  --end
 
-  -- Automatically format if the server supports it
-  --if client.resolved_capabilities.document_formatting then
-    --vim.api.nvim_command [[augroup Format]]
-    --vim.api.nvim_command [[autocmd! * <buffer>]]
-    --vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    --vim.api.nvim_command [[augroup END]]
-  --end
 end
-
--- Add the on_attach function to each server configuration (if applicable)
-vim.lsp.config('pyright', { on_attach = on_attach })
-vim.lsp.config('lua_ls', { on_attach = on_attach })
-vim.lsp.config('html', { on_attach = on_attach })
-vim.lsp.config('clangd', { on_attach = on_attach })
-vim.lsp.config('texlab', { on_attach = on_attach })
-vim.lsp.config('verible', { on_attach = on_attach })
-vim.lsp.config('rust_analyzer', { on_attach = on_attach })
-vim.lsp.config('ts_ls', { on_attach = on_attach })
-vim.lsp.config('cssls', { on_attach = on_attach })
