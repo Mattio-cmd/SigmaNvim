e3beb619c39975d6700acc1e39ffdc12e8e57697
Mattio-cmd <mattioc@protonmail.com>
2026-04-03 23:53:46 -0600

    Added a session manager

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -32,6 +32,20 @@ return {
       priority = 1000,
   },
 
+  {
+      "gennaro-tedesco/nvim-possession",
+      dependencies = {
+          "ibhagwan/fzf-lua",
+      },
+      config = true,
+      keys = {
+          { "<leader>sl", function() require("nvim-possession").list() end, desc = "📌list sessions", },
+          { "<leader>sn", function() require("nvim-possession").new() end, desc = "📌create new session", },
+          { "<leader>su", function() require("nvim-possession").update() end, desc = "📌update current session", },
+          { "<leader>sd", function() require("nvim-possession").delete() end, desc = "📌delete selected session"},
+      },
+  },
+
 
   -- lualine
 {
